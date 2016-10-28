a41ed9dfae6feaf4730e38949a9155cda0372d91
Mark Battersby <mark@mojotech.com>
2016-10-28 12:11:36 -0400

    Testing

---
+++ app.js
@@ -0,0 +1,42 @@
+var Globe = window.Globe
+var Data = window.Data
+var div = document.getElementById('globe')
+var dataset = Data.results.bindings
+var urls = {
+  earth: 'img/world.jpg',
+  bump: 'img/bump.jpg',
+  specular: 'img/specular.jpg'
+}
+
+// create a globe
+var globe = new Globe(div, urls)
+
+// start it
+globe.init()
+
+var draw = function(i) {
+  var point = dataset[i]
+  var [lon, lat] = point.coord.value.replace('Point(', '').replace(')', '').split(' ')
+  var d = {
+    color: '#'+Math.floor(Math.random()*16777215).toString(16),
+    size: 5,
+    lat: lat,
+    lon: lon
+  }
+
+  globe.center(d)
+
+  setTimeout(function() {
+    // offset the lat/long so you can actually
+    // see the block levitating
+
+    d.lat += 10
+    d.lon += 10
+
+    globe.addLevitatingBlock(d)
+  }, 300)
+
+  setTimeout(function() { return draw(i+1) }, 1000)
+}
+
+draw(0)
